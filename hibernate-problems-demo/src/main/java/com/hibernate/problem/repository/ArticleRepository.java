26af8df73a9066da230fbb3a3bd95f5b04223448
TarasVovk669 <taras.ivschk@gmail.com>
2024-11-20 17:36:01 +0100

    hibernate common problem

--- hibernate-problems-demo/src/main/java/com/hibernate/problem/repository/ArticleRepository.java
+++ hibernate-problems-demo/src/main/java/com/hibernate/problem/repository/ArticleRepository.java
@@ -1,7 +1,6 @@
 package com.hibernate.problem.repository;
 
 import com.hibernate.problem.domain.Article;
-import com.hibernate.problem.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- hibernate-problems-demo/src/main/java/com/hibernate/problem/repository/AuthorRepository.java
+++ hibernate-problems-demo/src/main/java/com/hibernate/problem/repository/AuthorRepository.java
@@ -4,7 +4,6 @@ import com.hibernate.problem.domain.Author;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
